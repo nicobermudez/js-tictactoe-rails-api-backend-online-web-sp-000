6f9cb6d270c8e81ae768c5916fde2d4f95c2bcfd
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-15 14:53:33 +0000

    Automatically backed up by Learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,19 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
 
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def body
+    #  to_json: 
+  end
 end
